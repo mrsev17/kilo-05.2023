44b185756b4db8c32b52b1dd2d5a52612e4d2949
mr.Seven <mr.upgrade3@protonmail.com>
2023-06-05 17:55:37 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,6 +2,24 @@
 
 // Datepicker //
 
+// $(function () {
+//     const today = new Date();
+//     const minDate = new Date(
+//         today.getFullYear(),
+//         today.getMonth(),
+//         today.getDate() + 7
+//     );
+
+//     $("#datepicker")
+//         .datepicker({
+//             minDate: minDate,
+//             dateFormat: "dd/mm/yy",
+//             dayNamesMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+//         })
+//         .datepicker("setDate", minDate);
+
+// });
+
 $(function () {
     const today = new Date();
     const minDate = new Date(
@@ -15,6 +33,19 @@ $(function () {
             minDate: minDate,
             dateFormat: "dd/mm/yy",
             dayNamesMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+            beforeShow: function (input, inst) {
+                setTimeout(function () {
+                    // Calculate the position of the input
+                    var inputOffset = $(input).offset();
+
+                    // Set the position of the datepicker calendar
+                    inst.dpDiv.css({
+                        top: inputOffset.top + $(input).outerHeight(),
+                        left: inputOffset.left,
+                        position: "fixed",
+                    });
+                }, 0);
+            },
         })
         .datepicker("setDate", minDate);
 });
