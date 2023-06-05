db9300736e5df79b3dca399cb63a9debb87bdd13
mr.Seven <mr.upgrade3@protonmail.com>
2023-06-05 15:18:39 +0300

    Update project
    Add datepicker in section banner

---
+++ script/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Datepicker //
+
+$(function () {
+    const today = new Date();
+    const minDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 7
+    );
+
+    $("#datepicker")
+        .datepicker({
+            minDate: minDate,
+            dateFormat: "dd/mm/yy",
+            dayNamesMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        })
+        .datepicker("setDate", minDate);
+});
