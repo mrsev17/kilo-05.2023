4fe4a22cdf39fea62176afa92e8a3497b0efb6e3
mr.Seven <mr.upgrade3@protonmail.com>
2023-06-07 21:22:04 +0300

    Update - 07.06.2023
    Update burger menu

--- script/script.js
+++ script/script.js
@@ -1,35 +1,7 @@
 "use strict";
 
-// $(function () {
-//     const today = new Date();
-//     const minDate = new Date(
-//         today.getFullYear(),
-//         today.getMonth(),
-//         today.getDate() + 7
-//     );
-
-//     $("#datepicker")
-//         .datepicker({
-//             minDate: minDate,
-//             dateFormat: "dd/mm/yy",
-//             dayNamesMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-//             beforeShow: function (input, inst) {
-//                 setTimeout(function () {
-//                     const inputOffset = $(input).offset();
-//                     inst.dpDiv.css({
-//                         top: inputOffset.top + $(input).outerHeight(),
-//                         left: inputOffset.left,
-//                         position: "fixed",
-//                     });
-//                 }, 0);
-//             },
-//         })
-//         .datepicker("setDate", minDate);
-// });
-
 const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-// Apply Safari-specific styles if the browser is Safari
 if (isSafari) {
     const elements = document.querySelectorAll(
         ".rectangle-home, .rectangle-moving-companies, .rectangle-reviews, .rectangle-cost-calculator, .rectangle-about-us"
@@ -111,67 +83,6 @@ document
     });
 
 // Code for tab menu //
-
-// const buttonHome = document.getElementById("tab-home-btn");
-// const blockHome = document.getElementById("tab-home-block");
-
-// const buttonMoving = document.getElementById("tab-moving-btn");
-// const blockMoving = document.getElementById("tab-moving-block");
-
-// const buttonReviews = document.getElementById("tab-reviews-btn");
-// const blockReviews = document.getElementById("tab-reviews-block");
-
-// const buttonCalculator = document.getElementById("tab-calculator-btn");
-// const blockCalculator = document.getElementById("tab-calculator-block");
-
-// const buttonAbout = document.getElementById("tab-about-btn");
-// const blockAbout = document.getElementById("tab-about-block");
-
-// function setupHoverFunction(buttonElement, blockElement) {
-//     let timer;
-
-//     buttonElement.addEventListener("mouseover", function () {
-//         clearTimeout(timer);
-//         blockElement.style.display = "block";
-//     });
-
-//     buttonElement.addEventListener("mouseout", function (event) {
-//         const e = event.toElement || event.relatedTarget;
-//         if (e && (e === blockElement || blockElement.contains(e))) {
-//             return;
-//         }
-//         timer = setTimeout(function () {
-//             blockElement.style.display = "none";
-//         }, 500); // Adjust the delay time (in milliseconds) as needed
-//     });
-
-//     blockElement.addEventListener("mouseover", function () {
-//         clearTimeout(timer);
-//     });
-
-//     blockElement.addEventListener("mouseout", function (event) {
-//         const e = event.toElement || event.relatedTarget;
-//         if (e && (e === buttonElement || buttonElement.contains(e))) {
-//             return;
-//         }
-//         timer = setTimeout(function () {
-//             blockElement.style.display = "none";
-//         }, 500); // Adjust the delay time (in milliseconds) as needed
-//     });
-// }
-
-// // Usage example:
-
-// setupHoverFunction(buttonHome, blockHome);
-// setupHoverFunction(buttonMoving, blockMoving);
-// setupHoverFunction(buttonReviews, blockReviews);
-// setupHoverFunction(buttonCalculator, blockCalculator);
-// setupHoverFunction(buttonAbout, blockAbout);
-
-// You can call the function again with different elements to achieve the same behavior for other buttons and blocks
-//   const buttonAnother = document.getElementById("another-btn");
-//   const blockAnother = document.getElementById("another-block");
-//   setupHoverFunction(buttonAnother, blockAnother);
 
 const buttonHome = document.getElementById("tab-home-btn");
 const blockHome = document.getElementById("tab-home-block");
@@ -264,9 +175,8 @@ function disableHoverFunction(pair) {
     });
 }
 
-// Check the window width and bind/unbind event listeners accordingly
 function checkWindowWidth() {
-    const minWidth = 992; // Set the minimum width here
+    const minWidth = 992;
 
     if (window.innerWidth >= minWidth) {
         buttonBlockPairs.forEach(enableHoverFunction);
@@ -275,142 +185,6 @@ function checkWindowWidth() {
     }
 }
 
-// Initial check on page load
 checkWindowWidth();
 
-// Check the window width on window resize
 window.addEventListener("resize", checkWindowWidth);
-
-// const buttonBlockPairs = [
-//     { button: buttonHome, block: blockHome },
-//     { button: buttonMoving, block: blockMoving },
-//     { button: buttonReviews, block: blockReviews },
-//     { button: buttonCalculator, block: blockCalculator },
-//     { button: buttonAbout, block: blockAbout },
-// ];
-
-// let activeBlock = null;
-// let timer;
-
-// function showBlock(blockElement) {
-//     clearTimeout(timer);
-
-//     if (activeBlock) {
-//         activeBlock.style.display = "none";
-//     }
-
-//     blockElement.style.display = "block";
-//     activeBlock = blockElement;
-// }
-
-// function handleMouseOver(buttonElement, blockElement) {
-//     showBlock(blockElement);
-// }
-
-// function handleMouseOut(buttonElement, blockElement) {
-//     timer = setTimeout(function () {
-//         blockElement.style.display = "none";
-//         activeBlock = null;
-//     }, 800); // Adjust the delay time (in milliseconds) as needed
-// }
-
-// // Check the window width before binding the event listeners
-// buttonBlockPairs.forEach(function (pair) {
-//     const minWidth = 992; // Set the minimum width here for each button-block pair
-
-//     if (window.innerWidth >= minWidth) {
-//         pair.button.addEventListener("mouseover", function () {
-//             handleMouseOver(pair.button, pair.block);
-//         });
-
-//         pair.button.addEventListener("mouseout", function () {
-//             handleMouseOut(pair.button, pair.block);
-//         });
-
-//         pair.block.addEventListener("mouseover", function () {
-//             clearTimeout(timer);
-//         });
-
-//         pair.block.addEventListener("mouseout", function () {
-//             handleMouseOut(pair.button, pair.block);
-//         });
-//     }
-// });
-
-// // Check the window width on window resize
-// window.addEventListener("resize", function () {
-//     if (window.innerWidth < 992 && activeBlock) {
-//         activeBlock.style.display = "none";
-//         activeBlock = null;
-//     }
-// });
-
-// const buttonBlockPairs = [
-//     { button: buttonHome, block: blockHome },
-//     { button: buttonMoving, block: blockMoving },
-//     { button: buttonReviews, block: blockReviews },
-//     { button: buttonCalculator, block: blockCalculator },
-//     { button: buttonAbout, block: blockAbout },
-// ];
-
-// let activeBlock = null;
-// let timer;
-
-// function showBlock(buttonElement, blockElement) {
-//     clearTimeout(timer);
-
-//     if (activeBlock) {
-//         activeBlock.button.classList.remove("active");
-//         activeBlock.block.style.display = "none";
-//     }
-
-//     buttonElement.classList.add("active");
-//     blockElement.style.display = "block";
-//     activeBlock = { button: buttonElement, block: blockElement };
-// }
-
-// function handleMouseOver(buttonElement, blockElement) {
-//     showBlock(buttonElement, blockElement);
-// }
-
-// function handleMouseOut(buttonElement, blockElement) {
-//     timer = setTimeout(function () {
-//         blockElement.style.display = "none";
-//         activeBlock = null;
-//     }, 2000); // Adjust the delay time (in milliseconds) as needed
-// }
-
-// // Check the window width before binding the event listeners
-// buttonBlockPairs.forEach(function (pair) {
-//     const minWidth = 992; // Set the minimum width here for each button-block pair
-
-//     if (window.innerWidth >= minWidth) {
-//         pair.button.addEventListener("mouseover", function () {
-//             handleMouseOver(pair.button, pair.block);
-//             pair.button.classList.add("active");
-//         });
-
-//         pair.button.addEventListener("mouseout", function () {
-//             handleMouseOut(pair.button, pair.block);
-//             pair.button.classList.remove("active");
-//         });
-
-//         pair.block.addEventListener("mouseover", function () {
-//             clearTimeout(timer);
-//         });
-
-//         pair.block.addEventListener("mouseout", function () {
-//             handleMouseOut(pair.button, pair.block);
-//             pair.button.classList.remove("active");
-//         });
-//     }
-// });
-
-// // Check the window width on window resize
-// window.addEventListener("resize", function () {
-//     if (window.innerWidth < 992 && activeBlock) {
-//         activeBlock.block.style.display = "none";
-//         activeBlock.button.classList.remove("active");
-//         activeBlock = null;
-//     }
-// });
